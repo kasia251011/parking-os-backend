dde75c6ee458d1d87939793346020df61eb7477f
cpprian <cpprian456@gmail.com>
2023-12-12 17:33:08 +0100

    create hello world api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post}, 
+    http::StatusCode,
+    Router, Json
+};
+use serde::{Deserialize, Serialize};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    // Enable tracing https://tokio.rs/#tk-lib-tracing
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "axum_api=debug".into())
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // TODO connect to mongodb
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("Listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
