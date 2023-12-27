5bf07ddeaf1c4a7837d4d30265ff2e86604c36ad
cpprian <cpprian456@gmail.com>
2023-12-27 07:36:55 +0100

    add tariff to update_ticket func

--- src/db/tariff.rs
+++ src/db/tariff.rs
@@ -1,7 +1,6 @@
-use std::str::FromStr;
-
 use bson::{oid::ObjectId, doc};
 use futures::StreamExt;
+use mongodb::Cursor;
 
 use crate::structs::{
     error::MyError::{*, self}, 
@@ -23,12 +22,7 @@ impl DB {
             .await
             .map_err(MongoQueryError)?;
 
-        let mut json_result: Vec<TariffResponse> = Vec::new();
-        while let Some(doc) = cursor.next().await {
-            json_result.push(self.doc_to_tariff(&doc.unwrap())?);
-        }
-
-        Ok(json_result)
+        Ok(self.cursor_to_vec(&mut cursor).await?)
     }
 
     pub async fn create_tariff(&self, body: &CreateTariffSchema) -> Result<String> {
@@ -55,18 +49,19 @@ impl DB {
         Ok("Successful operation".to_string())
     }
 
-    pub async fn get_tariff_by_id(&self, tariff_id: &str) -> Result<Tariff> {
-        let filter = doc! { "_id": ObjectId::from_str(tariff_id).unwrap() };
-        let tariff = self
+    pub async fn get_tariffs_by_parking_lot_id_ascending(&self, parking_lot_id: &str) -> Result<Vec<TariffResponse>> {
+        let filter = doc! { 
+            "parking_lot_id": parking_lot_id,
+            "$sort": { "min_time": 1 }
+        };
+
+        let mut cursor = self
             .tariff_collection
-            .find_one(filter, None)
+            .find(filter, None)
             .await
             .map_err(MongoQueryError)?;
 
-        match tariff {
-            Some(tariff) => Ok(tariff),
-            None => Err(MongoNotFound("Tariff not found".to_string())),
-        }
+        Ok(self.cursor_to_vec(&mut cursor).await?)
     }
 
     fn doc_to_tariff(&self, tariff: &Tariff) -> Result<TariffResponse> {
@@ -76,5 +71,14 @@ impl DB {
             max_time: tariff.max_time,
             price_per_hour: tariff.price_per_hour,
         })
+    }
+
+    async fn cursor_to_vec(&self, cursor: &mut Cursor<Tariff>) -> Result<Vec<TariffResponse>> {
+        let mut json_result: Vec<TariffResponse> = Vec::new();
+        while let Some(doc) = cursor.next().await {
+            json_result.push(self.doc_to_tariff(&doc.unwrap())?);
+        }
+
+        Ok(json_result)
     }
 }
